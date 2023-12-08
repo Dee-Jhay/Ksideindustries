eb199f230ccf8acaa467e24c0eba5c96cc5a0fd7
DeeJhay <samueldivine2021@gmail.com>
2023-12-08 16:44:26 +0100

    my first commit

---
+++ form-handler.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Process the form data
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+
+    // Perform any additional processing or validation here
+
+    // Email details
+    $to = 'samueldivine2021@gmail.com';  // Replace with your company's email address
+    $subject = 'New Form Submission';
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Email body
+    $email_body = "User Name: $username\n" .
+                  "User Email: $email\n" .
+                  "Phone: $phone\n" .
+                  "Message: $message\n";
+
+    // Send the email
+    $mail_success = mail($to, $subject, $email_body, $headers);
+
+    // Check if the email was sent successfully
+    if ($mail_success) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Oops! Something went wrong and we couldn't send your message.";
+    }
+}
+?>
---
+++ main.js
@@ -0,0 +1,40 @@
+//Get elements from the DOM
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const counters = document.querySelectorAll(".counter");
+
+//Toggle open/close menu
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("menu-open");
+});
+
+//select all counters
+counters.forEach(counter =>{
+    //set counter values to zero
+    counter.innerText = 0;
+    /*Set count variable to track the count*/
+    let count = 0;
+
+
+    //update count function
+    function updateCount() {
+        //Get counter target number to count to
+        const target = parseInt(counter.dataset.count);
+        //As long as the count is below the target number
+        if(count < target) {
+            //Increment the count by 10
+            count += 10;
+            //set the counter text to the count
+            counter.innerText = count + "+";
+            //Repeat this every 10 miliseconds
+            setTimeout(updateCount, 10); /*count speed*/
+            //And when the target is reached
+        } else {
+            //set the counter text to the target number
+            counter.innerText = target + "+";
+        }
+        
+    }
+    //run the function initially
+    updateCount();
+});
